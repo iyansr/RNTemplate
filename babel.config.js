931cd334dbc84a17a4975ea33ed1557ee5eb8899
IyanSR <ikhyan7@gmail.com>
2023-02-09 22:27:25 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@modules': './modules/src',
+        },
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.native',
+          '.native.js',
+          '.native.json',
+          '.native.ts',
+          '.native.tsx',
+        ],
+      },
+    ],
+  ],
+};
